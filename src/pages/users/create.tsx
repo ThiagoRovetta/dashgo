ce0ee549c580554c6c9a387d638fcc2766d61b07
ThiagoRovetta <rovetta.thiago@gmail.com>
2021-10-06 23:01:28 -0300

    Made users pages responsive.

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,5 +1,6 @@
 import { Box, Flex, Heading, Divider, VStack, SimpleGrid, HStack, Button } from '@chakra-ui/react'
 import { Input } from '../../components/Form/Input';
+import Link from 'next/link'
 
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
@@ -18,18 +19,18 @@ export default function Createuser() {
       >
         <Sidebar />
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+        <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
           <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
 
           <Divider my="6" borderColor="gray.700" />
 
           <VStack spacing="8">
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
               <Input name="name" label="Nome completo" />
               <Input name="email" type="email" label="E-mail" />
             </SimpleGrid>
             
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
               <Input name="password" type="password" label="Senha" />
               <Input name="password_confirmation" type="password" label="Confirmação da senha" />
             </SimpleGrid>
@@ -37,7 +38,9 @@ export default function Createuser() {
 
           <Flex mt="8" justify="flex-end">
             <HStack spacing="4">
-              <Button colorScheme="whiteAlpha">Caneclar</Button>
+              <Link  href="/users" passHref>
+                <Button as="a" colorScheme="whiteAlpha">Caneclar</Button>
+              </Link>
               <Button colorScheme="pink">Salvar</Button>
             </HStack>
           </Flex>
