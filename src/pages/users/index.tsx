ef2aa7c3e35f93ad43a95399a8af05afde8dfc85
ThiagoRovetta <rovetta.thiago@gmail.com>
2021-12-21 21:05:11 -0300

    Added axios and implemented loading when refetching data.

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -2,16 +2,16 @@ import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import Link from 'next/link'
 import { useQuery } from 'react-query'
+import { useEffect } from 'react';
 
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
 import { Pagination} from '../../components/Pagination';
-import { useEffect } from 'react';
+import { api } from '../../services/api';
 
 export default function Userlist() {
-  const { data, isLoading, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users')
-    const data = await response.json()
+  const { data, isLoading, error, isFetching } = useQuery('users', async () => {
+    const { data } = await api.get('/users')
 
     const users = data.users.map(user => ({
       id: user.id,
@@ -55,7 +55,11 @@ export default function Userlist() {
 
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+
+              {!isLoading && isFetching && <Spinner size='sm' color='gray.500' ml='4'/>}
+            </Heading>
 
             <Link href="/users/create" passHref>
               <Button
@@ -69,7 +73,6 @@ export default function Userlist() {
               </Button>
             </Link>
           </Flex>
-
           {isLoading ? (
               <Flex justify="center">
                 <Spinner />
@@ -126,7 +129,6 @@ export default function Userlist() {
                 </>
             )
           }
-          
         </Box>
       </Flex>
     </Box>
