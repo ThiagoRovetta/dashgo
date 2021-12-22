6bcd0323e28ff43e0cc05c6d7b10898815215ddc
ThiagoRovetta <rovetta.thiago@gmail.com>
2021-12-22 08:05:43 -0300

    Implemented react query useMutations and cache invalidation when creating a new user.

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -3,10 +3,14 @@ import Link from 'next/link'
 import { useForm, SubmitHandler } from 'react-hook-form'
 import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
+import { useMutation } from 'react-query'
 
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
 import { Input } from '../../components/Form/Input';
+import { api } from '../../services/api'
+import { queryClient } from '../../services/queryClient.ts'
+import { useRouter } from 'next/router'
 
 type CreateUserFormData = {
   name: string;
@@ -25,14 +29,30 @@ const createUserFormSchema = yup.object({
 })
 
 export default function Createuser() {
+  const router = useRouter();
+
+  const createUser = useMutation(async (user: CreateUserFormData) => {
+    const response = await api.post('users', {
+      user: {
+        ...user,
+        created_at: new Date(),
+      }
+    })
+
+    return response.data.user;
+  }, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('users')
+    }
+  });
+
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(createUserFormSchema)
   })
 
   const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-    await new Promise(resolve => setTimeout(resolve, 2000))
-
-    console.log('values', values)
+    await createUser.mutateAsync(values);
+    router.push('/users')
   }
 
   return (
--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -2,7 +2,7 @@ import { useQuery } from "react-query";
 import { api } from "../api";
 
 type User = {
-  id: number;
+  id: string;
   name: string;
   email: string;
   createdAt: string;
@@ -26,7 +26,7 @@ export async function getUsers(page: number): Promise<GetUsersResponse> {
       id: user.id,
       name: user.name,
       email: user.email,
-      createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+      createdAt: new Date(user.created_at).toLocaleDateString('pt-BR', {
         day: '2-digit',
         month: 'long',
         year: 'numeric'
